b353921905705908992d239b1a7cbe2720071830
pratham rathore <prathamrathore2004@gmail.com>
2025-11-28 14:19:20 +0530

    fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true,
+  },
+  env: {
+    ADMIN_USERNAME: process.env.ADMIN_USERNAME,
+    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+    
   },
 };
 
